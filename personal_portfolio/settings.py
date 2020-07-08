4a40910d8f9e56211e8d2a2773b6d9fe699e3904
Marcondes Amorim <marcondesjra@gmail.com>
2020-07-08 02:35:59 +0000

    Remove old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y4vbz#bv+_!o!ak3_g1oi%aswfh=q6k$km1n#9n^scveibh*$v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['marcondesamorim.pythonanywhere.com']
 
 # Application definition
 
